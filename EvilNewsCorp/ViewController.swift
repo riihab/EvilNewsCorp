19b5e1ad25a631fe652152bc04566a3f24b4bcbe
riihab <rihab.mehboob1@gmail.com>
2024-05-11 09:24:58 +0100

    Initial Commit

---
+++ EvilNewsCorp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EvilNewsCorp
+//
+//  Created by Rihab Mehboob on 11/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
